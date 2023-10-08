7498bc957de81a281cb1f5f354e040c788f0ef10
gabrielg08 <68970721+gabrielg08@users.noreply.github.com>
2023-10-08 15:09:46 -0400

    Add files via upload

---
+++ src/components/ResidentList.jsx
@@ -0,0 +1,39 @@
+import React, { useDebugValue, useEffect, useState } from "react";
+import ResidentCard from "./ResidentCard";
+import { paginationLogic } from "../utils/pagination";
+
+const ResidentList = ({ residents }) => {
+  
+  const [currentPage, setCurrenPage] = useState(1)
+  
+  const {pages, residentsInPage} = paginationLogic(currentPage, residents)
+
+  useEffect(() =>{
+    setCurrenPage(1)
+  }, [residents])
+
+  return (
+    <section>
+    <section className="grid grid-cols-[repeat(auto-fit,_300px)] justify-center gap-6 max-w-[1000px] mx-auto py-10">
+      {residentsInPage.map((resident) => (
+        <ResidentCard key={resident} residentEndpoint={resident} />
+      ))}
+    </section>
+
+    {/*Paginacion*/}
+        <ul className="text-leg flex gap-3 justify-center flex-wrap">
+          {
+            pages.map((page) =>(
+              <li key={page}>
+                <button className={`bg-white text-black p-2 ${page === currentPage && "bg-green-300"} mb-4 rounded-s`} onClick={() => setCurrenPage(page)}>
+                  {page}
+                </button>
+              </li>
+            ))
+          }
+        </ul>
+    </section>
+  );
+};
+
+export default ResidentList;
